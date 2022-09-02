d18c49a966eb187d731d8ef679a61433f7ffcfc7
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-09-02 21:02:49 +0530

    mockup data

---
+++ server/Data/mockData.js
@@ -0,0 +1,38 @@
+export const menu = [
+    {
+        id: 1,
+        identifier: 'popcorn',
+        name: 'Chicken popcorn',
+        img: 'https://t3.ftcdn.net/jpg/03/81/86/94/360_F_381869494_crVUl4wnYNsdhpdgJYYhtEhex0OTGxyj.jpg',
+        price: 499
+    },
+    {
+        id: 2,
+        identifier: 'burger',
+        name: 'Chicken burger',
+        img: 'https://thescranline.com/wp-content/uploads/2022/02/crispy-chicken-burger-WEB-01.jpg',
+        price: 299
+    },
+    {
+        id: 3,
+        identifier: 'wings',
+        name: 'Chicken wings',
+        img: 'https://www.licious.in/blog/wp-content/uploads/2020/12/Chicken-Wings-min.jpg',
+        price: 399
+    },
+    {
+        id: 4,
+        identifier: 'boneless',
+        name: 'Chicken boneless',
+        img: 'https://i.ytimg.com/vi/jYBGEZZL6xw/maxresdefault.jpg',
+        price: 599
+    },
+    {
+        id: 5,
+        identifier: 'bucket',
+        name: 'Bucket chicken',
+        img: 'https://5.imimg.com/data5/OB/AK/GLADMIN-33179691/chicken-buckets-500x500.png',
+        price: 999
+    },
+
+];
