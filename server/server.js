7be5c2b6ab044c614821120cd6ac13bafa1074a1
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-09-03 23:40:13 +0530

    changed method for using data

--- server/server.js
+++ server/server.js
@@ -23,12 +23,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
-app.post("/addmenu", addMenuToKfc);
-app.get("/menulist", getAllMenu);
-// app.get('/menu/:identifier', getMenuById);
-app.get("/searchmenu", getMenuById);
-app.put("/updatemenu", updateMenuById);
-app.delete("/deletemenu", deleteMenuById);
+app.post("/addmenu", addMenuToKfc); //can add new menu item
+app.get("/menulist", getAllMenu); //can be used to see the existing menu
+app.get("/searchmenu/:identifier", getMenuById);
+app.put("/updatemenu/:identifier", updateMenuById);
+app.delete("/deletemenu/:identifier", deleteMenuById);
 
 // DB connection establishment
 // mongoose.connect("mongodb://localhost:27017/kfcdata").then((res) => {
