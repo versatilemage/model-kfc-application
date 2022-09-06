6c95a24abde650e30a6742860f206c8653d4106a
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-09-06 22:48:52 +0530

    Update UpdateMenu.js

--- client/src/Components/UpdateMenu.js
+++ client/src/Components/UpdateMenu.js
@@ -1,8 +1,28 @@
 import React from 'react'
 
+import { getupdateData } from '../redux/updatemenu';
+
+import {useSelector, useDispatch} from 'react-redux';
+
 function UpdateMenu() {
+
+  const state = useSelector((state) => state)
+  const dispatch = useDispatch()
+  const name = state['KfcaddData']['name']
+  const identifier = state['KfcaddData']['identifier']
+  const price = state['KfcaddData']['price']
+  const img = state['KfcaddData']['img']
+  const loading = state['KfcaddData']['loading']
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // dispatch(getupdateData())
+  }
   return (
-    <div>UpdateMenu</div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder='enter name'/>
+      <button>search</button>
+    </form>
   )
 }
 
