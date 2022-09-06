e7e06cc56a0ad0d96e901def5cbf69c4c4c0890a
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-09-06 22:49:33 +0530

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,14 +1,21 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
 import menuReducer from "./redux/addNewmenu";
+
+import erasingreducer from "./redux/deleteSlice";
 
 import IdentifierReducer from "./redux/kfcIdentifierSlice";
 
 import KFCReducer from "./redux/kfcSlice";
 
+import updatedReducer from "./redux/updatemenu";
+
 const rootReducer = combineReducers({
   kfc: KFCReducer,
   kfcIdentifier: IdentifierReducer,
-  KfcaddData: menuReducer
+  KfcaddData: menuReducer,
+  kfcupdate: updatedReducer,
+  delKfc: erasingreducer
 });
 
 const store = configureStore({
