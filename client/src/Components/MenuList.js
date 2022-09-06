787c924bc300baa4d8d80b05c91b322df186c5ac
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-09-06 22:48:42 +0530

    Update MenuList.js

--- client/src/Components/MenuList.js
+++ client/src/Components/MenuList.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import React, { useEffect, useState } from 'react';
 
 import {useSelector, useDispatch} from 'react-redux';
@@ -14,17 +16,24 @@ function MenuList() {
     
   }, []);
 
+  const deletefood = (identifier) => {axios.delete(`http://localhost:4001/deletemenu/${identifier}`)}
+
   return (
-    <div className="menu-container">
+    <div className="grid grid-cols-3 items-baseline bg-red-800 space-y-5 justify-evenly py-10">
       {loading && <div>Please wait Loading.....</div>}
       {
         !loading && menu.length > 0 && menu.map((item) => (
-          <div className="menu-item--container" key={item.id}>
-            <img src={item.img}/>
-              <div className='menu-description'>
+          <div className="flex flex-col items-center m-10 bg-red-600 rounded-lg p-5 justify-items-start shadow shadow-black space-y-5
+          transition duration-700 hover:scale-105" key={item.id}>
+            <img src={item.img} className="w-72 h-64 rounded-lg"/>
+              <div className="text-xl font-bold text-white capitalize flex flex-col items-start w-64 text-start">
                 <div>{item.name}</div>
                 <div>${item.price}</div>
-                <button>Add</button>
+              </div>
+              <div className="flex flex-row space-x-5">
+                <button className="text-xl font-bold text-white uppercase bg-blue-700 rounded-lg px-6 py-1 w-10/12">update</button>
+                <button className="text-xl font-bold text-white uppercase bg-gray-500 rounded-lg px-6 py-1 w-10/12"
+                onClick={() => deletefood(item.identifier)}>delete</button>
               </div>
           </div>
         ))
