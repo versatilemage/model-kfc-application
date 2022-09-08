162b475fbadda8a6a28faaacb5d7c07e11af5ed7
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-09-08 19:27:13 +0530

    Update routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,14 @@
 import {KFCModel} from '../Models/KfcModel.js';
+import Jwt from 'jsonwebtoken';
+const {sign, verify} = Jwt
+const ACCESS = "4da4e435a4455093aff344937e824f2938b79620fc04ae3693443e1391c2b01b1cbf0c609178669f79c4fa27e40c7744e7e25f4b7bed6bede36b4914b48b13fa"
+import('dotenv')
 
 export const addMenuToKfc = async (req, res) => {
-    const identifier = req.body.identifier
-    const price = req.body.price
-    const name = req.body.name
-    const img = req.body.img
+    const {identifier, price, name, img} = req.body
+    // const menu = {unique: identifier}
+    // const expirey = {expiresIn: "2hr"}
+    const accessToken = sign({identifier: identifier},ACCESS, {expiresIn: "2hr"})
     const data = new KFCModel({
             price: price,
             name: name,
@@ -18,9 +22,17 @@ export const addMenuToKfc = async (req, res) => {
         res.send({
             status: 200,
             message: 'Data saved!!',
-            data: data
+            data: data,
+            Token: accessToken
+            // json({accessToken: accessToken})
+            // accessToken: json(accessToken)
         })
     })
+    // try{
+    //     await data.save()
+    // }catch (err) {
+    //     console.log(err)
+    // }
 }
 
 export const getAllMenu = (req, res) => {
@@ -59,11 +71,28 @@ export const getMenuById = (req, res) => {
 
 }
 
+export const tokenVerification = (req, res, next) => {
+    const headers = req.headers[`authorization`]
+    const token = headers.split(" ")[1]
+    verify(String(token), ACCESS, (err, data) => {
+        if (err) {
+            return res.send({
+                status: 400,
+                message: "invalid token used"
+            })
+        }
+        else {
+            res.send ({
+                status: 200,
+                data,
+                message: "menu is present"
+            })
+        }
+    })
+}
 export const updateMenuById = async(req, res) => {
+    const {price, name, img} = req.body
     const upid = req.params.identifier
-    const price = req.body.price
-    const name = req.body.name
-    const img = req.body.img
     KFCModel.findOneAndUpdate({identifier: upid},
         {$set:{
             price: price,
