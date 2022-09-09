be71426886462dbf492b2ef950130bd79e872c20
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-09-09 18:16:21 +0530

    Update UpdateMenu.js

--- client/src/Components/UpdateMenu.js
+++ client/src/Components/UpdateMenu.js
@@ -4,14 +4,16 @@ import Axios from "axios";
 
 import { getupdateData } from '../redux/updatemenu';
 
+import { loading, name, identifier, price, img } from '../redux/updatemenu';
+
 import { useNavigate } from 'react-router-dom';
 
-import { useSelector, useDispatch } from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 
 function UpdateMenu() {
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  // const {loading} = useSelector((state) => state.KfcUpdate)
+  // const {loading, name, identifier, price, img} = useSelector((state) => state.KfcUpdate)
   // const state = useSelector((state) => state)
   // const name = state['KfcUpdate']['name']
   const [initial, setinitial] = useState("")
@@ -21,10 +23,20 @@ function UpdateMenu() {
 
   const handleSubmit = (e) => {
     // setinitial(e.target.elements[0].value)
+    e.preventDefault()
+    const postData = {
+      name: foodname,
+      identifier: initial,
+      price: foodprice,
+      img: imageurl
+    }
+    dispatch(getupdateData(postData))
+    navigate("/home")
   }
 
   const handleAllupdate = (e) => {
     Axios.put(`http://localhost:4001/updatemenu/${initial}`,{
+      identifier: initial,
       name: foodname,
       img: imageurl,
       price: foodprice
@@ -34,15 +46,7 @@ function UpdateMenu() {
   return (
     <>
       <div className="flex flex-row justify-center items-center w-full p-32">
-        {/* <form onSubmit={handleSubmit} className="flex flex-col p-10 bg-red-800 h-80 py-28 space-y-4 shadow hover:shadow-black items-center">
-          <input type="text" placeholder="enter identifier" className="text-center py-3 px-2"/>
-          <button className="capitalize text-xl font-bold bg-green-800 text-white
-            rounded-lg px-3 py-2 w-3/5" type="submit">
-            search
-          </button>
-        </form> */}
-        {/* {loading && <div>Please wait Loading.....</div>} */}
-        <form className="flex flex-col w-full h-80 px-10 space-y-4 shadow hover:shadow-black py-10 rounded-sm bg-red-700 w-2/5 items-center"
+        <form className="flex flex-col px-10 space-y-4 shadow hover:shadow-black py-10 rounded-sm bg-red-700 w-2/5 items-center"
         onSubmit={handleAllupdate}>
         <input type="text" placeholder="enter identifier" className="text-center py-3 px-2 w-72"
           onChange={(e) => {setinitial(e.target.value)}}/>
