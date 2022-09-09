c0e99cfa7252704267bc40129d071d8cdda0420a
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-09-09 18:16:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 const PORT = 4001;
 const app = express();
 
@@ -11,7 +12,8 @@ import {
   getMenuById,
   updateMenuById,
   deleteMenuById,
-  tokenVerification
+  tokenVerification,
+  getMenu
 } from "./routes/routes.js";
 
 
@@ -23,21 +25,15 @@ app.use(bodyParser.json());
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cookieParser());
+
 // Routes
 app.post("/addmenu", addMenuToKfc); //can add new menu item
 app.get("/menulist", getAllMenu); //can be used to see the existing menu
 app.get("/searchmenu/:identifier", getMenuById);
 app.put("/updatemenu/:identifier", updateMenuById);
 app.delete("/deletemenu/:identifier", deleteMenuById);
-app.get("/verification", tokenVerification);
-
-// DB connection establishment
-// mongoose.connect("mongodb://localhost:27017/kfcdata").then((res) => {
-//   console.log("DB connect success");
-//   app.listen(PORT, () => {
-//     console.log(`Server listening at ${PORT}`);
-//   });
-// });
+app.get("/verification", tokenVerification, getMenu);
 
 mongoose.connect("mongodb+srv://wildbadger:jkhinpiqosq@clusterbankapp.pn7rge3.mongodb.net/?retryWrites=true&w=majority",{
   useNewUrlParser: true,
